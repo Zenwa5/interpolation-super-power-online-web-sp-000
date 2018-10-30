918340109b10798b9ac3e4d33af0874c8e87cfb9
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-10-30 23:08:41 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,5 @@ def display_rainbow(colors)
 
 end
 
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+colors = ['R: red', 'O: orange', 'Y: yellow', 'G: green', 'B: blue', 'I: indigo', 'V: violet']
+
